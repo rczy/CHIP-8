06aa8a0fd1afb6ce3ce0b8e3746cd10e12d0ca72
Bérczy Bálint <berczy.balint@gmail.com>
2024-11-17 14:45:40 +0100

    Beeper modified: - sound is generated on the fly - buffer removed

--- src/beeper.c
+++ src/beeper.c
@@ -7,26 +7,25 @@ beeper_t *beeper_create()
 {
     beeper_t *beeper = malloc(sizeof(beeper_t));
     beeper->state = MUTED;
-    beeper_buffer_init(beeper);
-    beeper->buffer_pos = 0;
+    beeper->period = SAMPLE_FREQ / TONE;
+    beeper->half_period = beeper->period / 2;
+    beeper->nth_chunk = 0;
 
-    SDL_AudioSpec *spec = malloc(sizeof(SDL_AudioSpec));
-    spec->freq = SAMPLE_FREQ;
-    spec->format = AUDIO_S16SYS;
-    spec->channels = 1;
-    spec->samples = SAMPLES;
-    spec->callback = beeper_callback;
-    spec->userdata = beeper;
-    beeper->spec = spec;
-
-    beeper->id = SDL_OpenAudioDevice(NULL, 0, spec, NULL, 0);
+    SDL_AudioSpec spec = {
+        .freq = SAMPLE_FREQ,
+        .format = AUDIO_S16SYS,
+        .channels = 1,
+        .samples = SAMPLES,
+        .callback = beeper_callback,
+        .userdata = beeper,
+    };
+    beeper->id = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
 
     return beeper->id ? beeper : NULL;
 }
 
 void beeper_destroy(beeper_t **beeper)
 {
-    free((*beeper)->spec);
     SDL_CloseAudioDevice((*beeper)->id);
     free(*beeper);
     *beeper = NULL;
@@ -50,23 +49,14 @@ void beeper_mute(beeper_t *beeper)
     beeper->state = MUTED;
 }
 
-void beeper_buffer_init(beeper_t *beeper)
-{
-    int period = SAMPLE_FREQ / TONE;
-
-    for (int i = 0; i < SAMPLE_FREQ; i++) {
-        beeper->buffer[i] = (i % period < period / 2) ? INT16_MIN : INT16_MAX;
-    }
-}
-
 void beeper_callback(void *userdata, uint8_t *stream, int len)
 {
     beeper_t *beeper = (beeper_t *)userdata;
     int16_t *out = (int16_t *)stream;
     for (int i = 0; i < len / 2; i++) {
-        out[i] = beeper->buffer[beeper->buffer_pos++];
-        if (beeper->buffer_pos == BUFFER_LENGTH) {
-            beeper->buffer_pos = 0;
+        out[i] = (beeper->nth_chunk++ % beeper->period < beeper->half_period) ? INT16_MIN : INT16_MAX;
+        if (beeper->nth_chunk == beeper->period) {
+            beeper->nth_chunk = 0;
         }
     }
 }
--- src/include/beeper.h
+++ src/include/beeper.h
@@ -6,17 +6,16 @@
 
 #define SAMPLE_FREQ 44100
 #define SAMPLES 2048
-#define BUFFER_LENGTH SAMPLE_FREQ
 #define TONE 440
 
 typedef enum beeper_state_t { BEEPING, MUTED } beeper_state_t;
 
 typedef struct beeper_t {
     SDL_AudioDeviceID id;
-    SDL_AudioSpec *spec;
     beeper_state_t state;
-    unsigned int buffer_pos;
-    int16_t buffer[BUFFER_LENGTH];
+    unsigned int period;
+    unsigned int half_period;
+    unsigned int nth_chunk;
 } beeper_t;
 
 beeper_t *beeper_create();
